64a549461de40baee7a3f37881881d30dc492aef
surya <suryaraghavarapu@gmail.com>
2023-06-30 03:12:00 -0400

    push the changes

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,43 +1,47 @@
-import { NextResponse } from "next/server";
-import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
-import { AxiosResponse } from "axios";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai';
+import { AxiosResponse } from 'axios';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export async function POST(request: Request, response: any) {
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
   try {
-    const { title, role } = await request.json();
+    // Check if the request method is POST
+    if (request.method !== 'POST') {
+      return response.status(405).end(); // Method Not Allowed
+    }
+
+    const { title, role } = await request.body;
 
     const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> =
       await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         messages: [
           {
-            role: "user",
+            role: 'user',
             // content: `Create small blog post with html tags based on this title: ${title}`,
             content: `Create a three line blog post with html tags based on this title: ${title}`,
           },
           {
-            role: "system",
+            role: 'system',
             content: `${
-              role || "I am a helpful assistant"
+              role || 'I am a helpful assistant'
             }. Write with html tags.`,
           },
         ],
       });
 
-    // response.revalidate("/api/posts")
-    return NextResponse.json(
-      {
-        content: aiResponse.data.choices[0].message?.content,
-      },
-      { status: 200 }
-    );
+    return response.status(200).json({
+      content: aiResponse.data.choices[0].message?.content,
+    });
   } catch (error) {
-    console.error("request error", error);
-    NextResponse.json({ error: "error updating post" }, { status: 500 });
-  } 
+    console.error('request error', error);
+    return response.status(500).json({ error: 'Error updating post' });
+  }
 }
